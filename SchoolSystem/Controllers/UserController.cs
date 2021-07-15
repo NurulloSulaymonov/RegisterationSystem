8593fe2bdbf1b418c4510769c37f32a8e010525f
NurulloSulaymonov <sulaimonovnn@gmail.com>
2021-07-15 10:16:57 +0500

    Started

---
+++ SchoolSystem/Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolSystem.Models.Services;
+using SchoolSystem.Models.ViewModel;
+
+namespace SchoolSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        private  UserService _userService;
+
+        public UserController()
+        {
+            _userService = new UserService();
+        }
+        // GET
+        public IActionResult Index()
+        {
+            return RedirectToAction("Login");
+        }
+        
+        
+        
+        //register get and post
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(new RegisterViewModel());
+        }
+        
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            //validity of model
+            if (ModelState.IsValid == false) return View(model);
+
+            var success = _userService.Register(model);
+            if (success == false) return View(model);
+          
+            return RedirectToAction("Login");
+
+        }
+        
+        
+        //Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+        
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid == false) return View(model);
+
+            var success = _userService.Login(model);
+            if (success == false) return View(model);
+
+            else return RedirectToAction("Success");
+
+        }
+
+        
+        //success 
+        public IActionResult Success()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ SchoolSystem/Models/Services/UserService.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
+using SchoolSystem.Models.ViewModel;
+
+namespace SchoolSystem.Models.Services
+{
+    public class UserService
+    {
+        public UserService()
+        {
+            
+        }
+        
+        
+        //register
+        public bool Register(RegisterViewModel model)
+        {
+            //get all list of users
+            var listofUsers = GetListOfUsers();
+            var userExists = listofUsers.Any(x => x.UserName == model.UserName);
+            if (userExists) return false; // if user with uesrname exists just return back
+            
+            var user = new User()
+            {
+                Password = model.Password,
+                Phone = model.Phone,
+                UserName = model.UserName
+            };
+            if (listofUsers.Count == 0)
+            {
+                user.Id = 1;
+            }
+            else
+            {
+                var last = listofUsers.OrderBy(x => x.Id).Last().Id;
+                user.Id = last + 1;
+            }
+
+            listofUsers.Add(user);
+
+            var text = JsonConvert.SerializeObject(listofUsers);
+            File.WriteAllText("users.json", text);
+
+            return true;
+        }
+        
+        
+        //login
+        public bool Login(LoginViewModel model)
+        {
+
+            var users = GetListOfUsers();
+
+            var success = users.Any(x => x.UserName == model.UserName && x.Password == model.Password);
+            return success;
+        }
+        
+        //list
+        public List<User> GetListOfUsers()
+        {
+            var users = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("users.json"));
+            return users;
+        }
+    }
+}
